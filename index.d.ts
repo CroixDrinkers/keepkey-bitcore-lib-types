dbc9e0d5137c371fa0e486ab19f71b1ffa751b3b
Ken Heutmaker & Kartik Iyer <tech@keepkey.com>
2016-12-19 12:35:07 -0800

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2015-2016 KeepKey, LLC
+ * All Rights Reserved
+ */
+export = Bitcore;
+
+declare module Bitcore {
+  class HDPublicKey {
+    constructor(arg: any);
+    derive(arg: string | number): HDPublicKey;
+    toObject(): HDPublicKeyDetails;
+  }
+
+  class PublicKey {
+    constructor(arg: string);
+    toAddress(network?: Network): Address;
+  }
+
+  class PrivateKey {
+    constructor();
+    static fromWIF(wif: string): PrivateKey;
+  }
+
+  class Network {
+
+  }
+
+  class Networks {
+    static dash: Network;
+  }
+
+  interface HDPublicKeyDetails {
+    network: string;
+    depth: number;
+    fingerPrint: number;
+    parentFingerPrint: number;
+    childIndex: number;
+    chainCode: string; //Hex
+    publicKey: string; //Hex
+    xpubkey: string; //base58
+    checksum: string; //base58
+  }
+
+  interface Address {
+    toString(): string;
+  }
+
+  class Message {
+    constructor(message: any);
+    verify(message: string, signature: string);
+    sign(privateKey: PrivateKey): string;
+  }}
+
+
+declare namespace Bitcore.encoding {
+  class Base58 {
+    constructor(obj: any);
+    static toString(): string;
+  }
+}
+
